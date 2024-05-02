faf89f2b05800088f842a81ba2f2b61feb08256b
Nyan <nyandude22@gmail.com>
2024-05-02 13:05:25 +0200

    Updated server

--- server.js
+++ server.js
@@ -2,25 +2,30 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-require('dotenv').config({ path:'.env-local' });
+require('dotenv').config();  // Ensures that the .env file is loaded into process.env
 
 const app = express();
-app.use(express.json());
-app.use(cors());
+app.use(express.json());  // Parses JSON request bodies
+app.use(cors());  // Enables CORS for all domains
 
 // Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/quotes', {
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 }).then(() => {
   console.log('Connected to MongoDB');
 }).catch(err => {
-  console.error('Could not connect to MongoDB', err);
+  console.error('Could not connect to MongoDB:', err);
 });
+
+// Log the MongoDB URI to confirm it is loaded correctly (can be removed in production)
+console.log("MongoDB URI: ", process.env.MONGO_URI);
 
 // Central API route prefix
 app.use('/api', require('./src/routes/authRoutes'));
 app.use('/api', require('./src/routes/quoteRoutes'));
 
-const port = 3001;
+const port = process.env.PORT || 3001; // Default to 3000 if PORT is not specified
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
